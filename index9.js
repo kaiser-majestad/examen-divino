a2c31b4c1856f59a5c36a350ea766e718c9f5cdf
kaiser-majestad <nelsonalzateg@donbosco.edu.co>
2024-11-27 18:25:09 -0500

    Add files via upload

---
+++ index9.js
@@ -0,0 +1,23 @@
+document.getElementById('myForm').onsubmit = function() {
+
+    var title = document.getElementById('title').value;
+    var body = document.getElementById('body').value;
+  
+     var data = {
+        title: title,
+        body: body,
+        userId: 1
+    };
+  
+    
+    fetch('JSONPlaceholder API', {
+      body: data  
+    })
+    .then(response => response.json())  
+    .then(post => {
+      var row = '<tr><td>' + post.id + '</td><td>' + post.title + '</td><td>' + post.body + '</td></tr>';
+      document.getElementById('post-lista').innerHTML += row;
+    })
+    .catch(error => console.log(error));  
+  };
+  
